0cf1a87d02f3c9949c7dcc87e98ae99d224ec479
flaviamirabal <fmirabal@deloitte.com>
2020-06-25 15:10:21 +0000

    laboratorio10.2

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -8,16 +8,17 @@ sap.ui.define([
 			var oMockServer = new MockServer({
 				/* eslint-disable sap-no-hardcoded-url */
 				rootUri: "https://services.odata.org/V2/Northwind/Northwind.svc/"
+				/* eslint-enable sap-no-hardcoded-url */
 			});
 
-			var oUriParameters = UriParameters.fromQuery(window.location.search);
+			var oUriParameters = new UriParameters.fromQuery(window.location.search);
 
 			MockServer.config({
 				autorespond: true,
-				autorespondAfter: oUriParameters.get("ServerDelay") || 500
+				autorespondAfter: oUriParameters.get("serverDelay") || 500
 			});
 
-			var sPath = "../localService/";
+			var sPath = "../localService";
 
 			oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
 			oMockServer.start();
--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -1,4 +1,9 @@
 sap.ui.define(["../localService/mockserver"],
 	function (mockserver) {
+		
+		//inicialización de servidor mock
 		mockserver.init();
+		
+		//inicialización de componente embebido para html
+		sap.ui.require(["sap/ui/core/ComponentSupport"]);
 	});
